33a72b17ac18de96c2e394a708a27e70b54f0aaa
Colin Gregory <cgregory.am@gmail.com>
2019-12-06 11:38:59 -0800

    99% finished

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 var path = require("path");
-var rooms = [];
+var rooms = ["General", "Private"];
 
 app.use(express.static(path.join(__dirname + "/public")));
 
@@ -18,6 +18,7 @@ app.get("/css/styles.css", function(req, res) {
 users = [];
 io.on("connection", function(socket) {
   console.log("A user connected");
+  console.log(rooms);
   socket.on("setUsername", function(data) {
     if (users.indexOf(data) < 0) {
       console.log("Setting Username: " + data);
@@ -33,8 +34,12 @@ io.on("connection", function(socket) {
   });
 
   socket.on("newRoom", function(data) {
-    rooms.push(data.name);
-    io.sockets.emit("addRoom", { name: data.name });
+    if (data.name === "") {
+      //do nothing
+    } else {
+      rooms.push(data.name);
+      io.sockets.emit("addRoom", { name: data.name });
+    }
   });
 
   socket.on("msg", function(data) {
