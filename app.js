cf3fbb5576ca7bc9ee6991339684f8d7e2cfcd57
Colin Gregory <cgregory.am@gmail.com>
2019-12-07 14:34:24 -0800

    added error handling for client

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 var path = require("path");
-var rooms = ["General", "Private"];
+var rooms = ["General"];
 
 app.use(express.static(path.join(__dirname + "/public")));
 
@@ -16,15 +16,15 @@ app.get("/css/styles.css", function(req, res) {
 });
 
 users = [];
+sockets = [];
 io.on("connection", function(socket) {
   console.log("A user connected");
-  console.log(rooms);
   socket.on("setUsername", function(data) {
     if (users.indexOf(data) < 0) {
       console.log("Setting Username: " + data);
       users.push(data);
+      sockets.push(socket);
       socket.emit("userSet", { username: data, avaliableRooms: rooms });
-      console.log(users);
     } else {
       socket.emit(
         "userExists",
@@ -36,7 +36,9 @@ io.on("connection", function(socket) {
   socket.on("newRoom", function(data) {
     if (data.name === "") {
       //do nothing
+      console.log("Room name empty, skipping insert");
     } else {
+      console.log("Creating room: " + data.name);
       rooms.push(data.name);
       io.sockets.emit("addRoom", { name: data.name });
     }
@@ -44,6 +46,40 @@ io.on("connection", function(socket) {
 
   socket.on("msg", function(data) {
     io.sockets.emit("newmsg", data);
+  });
+
+  socket.on("connection_failed", function() {
+    console.log("connection failed");
+    document.write("I am sorry. The server is having troubles.");
+  });
+
+  socket.on("connect_error", function() {
+    console.log("connect error");
+    document.write("I am sorry. The server is having troubles.");
+  });
+
+  socket.on("reconnect_failed", function() {
+    console.log("reconnect failure");
+    document.write("I am sorry. The server is having troubles.");
+  });
+
+  socket.on("connect_error", function() {
+    console.log("connect error");
+    document.write("I am sorry. The server is having troubles.");
+  });
+
+  socket.on("connect_timeout", function() {
+    console.log("connection timeout");
+    document.write("I am sorry. The server is having troubles.");
+  });
+
+  socket.on("disconnect", function() {
+    console.log("Disconnect");
+    var i = sockets.indexOf(socket);
+    users.splice(i, 1);
+    sockets.splice(i, 1);
+    console.log("User list after disconnect:");
+    console.log(users);
   });
 });
 
